e5d535075a534dc2f9101a939a731940f1e18790
HyunSu1768 <azxcv1768@gmail.com>
2024-12-03 22:37:43 +0900

    feat(giftcardcharge): 기프트 카드 컨트롤러 및 서비스 추가 기능(giftcardcharge): 기프트 카드 관련 로직을 처리하기 위한 컨트롤러와 서비스 클래스를 생성하여 구조를 확장한다.

---
+++ src/main/kotlin/com/cuberlabs/cuperpinserver/domain/giftcardcharge/controller/GiftCardController.kt
@@ -0,0 +1,4 @@
+package com.cuberlabs.cuperpinserver.domain.giftcardcharge.controller
+
+class GiftCardController {
+}
---
+++ src/main/kotlin/com/cuberlabs/cuperpinserver/domain/giftcardcharge/service/GiftCardService.kt
@@ -0,0 +1,4 @@
+package com.cuberlabs.cuperpinserver.domain.giftcardcharge.service
+
+class GiftCardService {
+}
