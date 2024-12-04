95b2fc5c8762a8e2578d35ff61a93432cbe3d84d
HyunSu1768 <azxcv1768@gmail.com>
2024-12-04 17:45:03 +0900

    feat(BankingService.kt): BankingService 클래스에 sendMoney 및 isFraudulentAccount 메서드 추가 은행 서비스의 기본 기능을 구현하기 위해 메서드를 추가함.

--- src/main/kotlin/com/cuberlabs/cuperpinserver/infrastructure/banking/BankingService.kt
+++ src/main/kotlin/com/cuberlabs/cuperpinserver/infrastructure/banking/BankingService.kt
@@ -1,4 +1,20 @@
 package com.cuberlabs.cuperpinserver.infrastructure.banking
 
-class BankingService {
+import com.cuberlabs.cuperpinserver.domain.banking.Banking
+import com.cuberlabs.cuperpinserver.domain.banking.BankingStatus
+import com.cuberlabs.cuperpinserver.domain.giftcardcharge.entity.vo.Bank
+import org.springframework.stereotype.Component
+
+@Component
+class BankingService(): Banking {
+    override fun sendMoney(amount: Int, bank: Bank, accountOwner: String): BankingStatus {
+        return BankingStatus(
+            isSuccess = true,
+            message = "Not yet implemented"
+        )
+    }
+
+    override fun isFraudulentAccount(accountNumber: String): Boolean {
+       return false
+    }
 }
