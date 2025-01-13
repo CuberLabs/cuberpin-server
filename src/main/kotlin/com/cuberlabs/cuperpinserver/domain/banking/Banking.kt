4bf56b1a94705a4ab86ae63948e99e819534ae4e
HyunSu1768 <azxcv1768@gmail.com>
2025-01-13 18:21:46 +0900

    chore(banking): Banking 관련 파일 삭제 불필요한 Banking 인터페이스와 BankingService 클래스를 제거하여 코드 정리.

--- src/main/kotlin/com/cuberlabs/cuperpinserver/domain/banking/Banking.kt
+++
@@ -1,8 +0,0 @@
-package com.cuberlabs.cuperpinserver.domain.banking
-
-import com.cuberlabs.cuperpinserver.domain.giftcardcharge.entity.vo.Bank
-
-interface Banking {
-    fun sendMoney(amount: Int, bank: Bank, accountOwner: String, accountNumber: String): BankingStatus
-    fun isFraudulentAccount(accountNumber: String): Boolean
-}
