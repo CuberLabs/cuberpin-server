c54d9f858751d261c70cb3a52f0d434d7a89311b
HyunSu1768 <azxcv1768@gmail.com>
2025-01-29 23:09:59 +0900

    fix(CorsConfig.kt): CORS 설정에 새로운 허용된 출처 추가 새로운 IP 주소(http://192.168.219.100:3000)를 허용하여 로컬 개발 환경에서의 접근성을 향상시킴.

--- src/main/kotlin/com/cuberlabs/cuperpinserver/infrastructure/config/security/CorsConfig.kt
+++ src/main/kotlin/com/cuberlabs/cuperpinserver/infrastructure/config/security/CorsConfig.kt
@@ -13,7 +13,7 @@ class CorsConfig(private val securityProperty: SecurityProperties) {
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration().apply {
-            allowedOrigins = listOf(securityProperty.frontDomain, securityProperty.backDomain, "http://localhost:3000")
+            allowedOrigins = listOf(securityProperty.frontDomain, securityProperty.backDomain, "http://localhost:3000", "http://192.168.219.100:3000")
             allowedMethods = listOf("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD")
             allowCredentials = true
             addAllowedHeader("*")
